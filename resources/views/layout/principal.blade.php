bbf2b6c3162d688b562ed30a71fad33c416fa217
Marcelo <marcelobuzzetti@gmail.com>
2018-01-31 18:13:55 -0200

    Pagina Inicial

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,95 +1,95 @@
- <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <!-- Compiled and minified CSS -->
-      <link rel="stylesheet" href="/css/bootstrap.min.css">
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Cautela</title>
-      <style>
-        /* Animacoes */
-        .animated {
-          animation-duration: 0.8s;
-          animation-fill-mode: both;
-        }
-
-        @keyframes fadeIn {
-          from {
-            opacity: 0;
+   <!DOCTYPE html>
+    <html>
+      <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Cautela</title>
+        <style>
+          /* Animacoes */
+          .animated {
+            animation-duration: 0.8s;
+            animation-fill-mode: both;
           }
 
-          to {
-            opacity: 1;
+          @keyframes fadeIn {
+            from {
+              opacity: 0;
+            }
+
+            to {
+              opacity: 1;
+            }
           }
-        }
 
-        .fadeIn {
-          animation-name: fadeIn;
-        }
-      </style>
-    </head>
-    <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+          .fadeIn {
+            animation-name: fadeIn;
+          }
+        </style>
+      </head>
+      <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Cautela</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-        @auth
-                        <li>
-                          <a class="nav-link" href="{{ url('/home') }}">Home</a>
-                        </li>
-                    @else
-                        <li>
-                          <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li>
-                          <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
-                    @endauth
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
           @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-    <div class="container  animated fadeIn">
-        @yield('content')
+                          <li>
+                            <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                          </li>
+                      @else
+                          <li>
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                          </li>
+                          <li>
+                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                          </li>
+                      @endauth
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            @auth
+                          <a href="{{ url('/home') }}">Home</a>
+                      @else
+                          <a href="{{ route('login') }}">Login</a>
+                          <a href="{{ route('register') }}">Register</a>
+                      @endauth
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
     </div>
-    
+  </nav>
 
-      <script type="text/javascript" src="/js/jquery.min.js"></script>
-      <script type="text/javascript" src="/js/popper.min.js"></script>
-      <script type="text/javascript" src="/js/bootstrap.min.js"></script>
-    </body>
-  </html>
+      <div class="container  animated fadeIn">
+          @yield('content')
+      </div>
+      
+
+        <script type="text/javascript" src="/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/popper.min.js"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+      </body>
+    </html>
