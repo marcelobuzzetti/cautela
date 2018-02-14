21ff2e3d91a5f1a6b84fc9cc74bb0ef53f3a057a
Marcelo <marcelobuzzetti@gmail.com>
2018-02-14 17:18:54 -0200

    Correcao detalhes cautela

--- resources/views/cautela/detalhes.blade.php
+++ resources/views/cautela/detalhes.blade.php
@@ -18,8 +18,6 @@
                   <th>Data de Cautela</th>
                   <th>Quantidade</th>
                   <th>Observação</th>
-                  <th>Alteração</th>
-                  <th>Entregar</th>
               </tr>
             </thead>
             <tfoot>
@@ -28,8 +26,6 @@
                   <th>Data de Cautela</th>
                   <th>Quantidade</th>
                   <th>Observação</th>
-                  <th>Alteração</th>
-                  <th>Entregar</th>
               </tr>
             </tfoot>
 
@@ -40,18 +36,6 @@
                 <td>{{$c->data_cautela}}</td>
                 <td>{{$c->quantidade}}</td>
                 <td>{{$c->observacao_cautela or "Nenhuma Observação"}}</td>
-                @if($c->data_entrega == NULL)
-                <form action="{{ action('CautelaMaterialController@entrega') }}" method="post">
-                  <input name="cautela" value="{{$c->cautela}}" type="hidden">
-                  <input type="hidden" id="id" name='id' value="{{$c->id}}"/>
-                  <input type="hidden" name="_token" value="{{{ csrf_token() }}}"/>
-                  <td><input class="form-control" type="text"  id="observacao_entrega" name="observacao_entrega" placeholder="Digite as alterações encontradas"></td>
-                <td><button type="submit" class="btn btn-danger">Entregar</button></td>
-                @else
-                <td>{{$c->observacao_entrega  or "Nenhuma Alteração"}}</td>
-                <td>Entregue em {{$c->data_entrega}}</td>
-                @endif
-                </form>
               </tr>
             @endforeach
             </tbody>
